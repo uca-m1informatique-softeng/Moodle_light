c63eb9709d85a292581bf9319d7602cd1c34e3d2
KuhlMatthis <matthis.kuhl@etu.univ-cotedazur.fr>
2022-02-03 16:57:11 +0100

    rajout fonctionalliter ActionsManager

--- src/main/java/Module.java
+++ src/main/java/Module.java
@@ -2,5 +2,11 @@ import java.util.ArrayList;
 
 public class Module {
     String name;
+    String id;
     ArrayList<Ressource> ressources = new ArrayList<Ressource>();
+
+    public Module(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
 }
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -7,15 +7,8 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
         boolean run = true;
-        while(run){
-            Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-            System.out.println("Choice :\n " +
-                    "0 nam : add module \n " +
-                    "1 nam text : create Text\n " +
-                    "2 nam text : create questionaire" );
-            String request = myObj.nextLine();
-            System.out.println("request is: " + request);
-
-        }
+        String request = "";
+        ActionsManager actionsManager = new ActionsManager();
     }
+
 }
