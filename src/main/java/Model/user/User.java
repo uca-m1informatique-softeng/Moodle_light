72df89c64b41b0c67c6f465a785ebe4790e864fa
KuhlMatthis <matthis.kuhl@etu.univ-cotedazur.fr>
2022-02-04 15:55:18 +0100

    rajout dependance javax

--- src/main/java/Model/user/User.java
+++ src/main/java/Model/user/User.java
@@ -1,8 +1,27 @@
 package Model.user;
 
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(	name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "username"),
+                @UniqueConstraint(columnNames = "id")
+        })
 public class User {
+
     public String name;
     public String id;
+
+    @NotBlank
+    @Size(max = 120)
+    private String password;
+
     public User(String name, String id) {
         this.name = name;
         this.id = id;
