f646146e146e1ab47b0de6df010378a376baa1b0
KuhlMatthis <matthis.kuhl@etu.univ-cotedazur.fr>
2022-02-05 11:09:32 +0100

    Spring boot
    enleve ActionManager
    mets a jour debut test(no error)
    rajoute default package main function

--- src/main/java/Model/Documents/Cours.java
+++ src/main/java/Model/Documents/Cours.java
@@ -1,14 +1,16 @@
 package Model.Documents;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.Set;
 
 @Entity
 @Table(	name = "cours")
 public class Cours extends Ressource{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
 
-
+    @OneToMany
     public Set<String> text;
 
 
--- src/test/java/ActionManagerTest.java
+++ src/test/java/ActionManagerTest.java
@@ -1,7 +1,7 @@
 
-import Model.ActionsManager;
 import Model.Documents.Cours;
 import Model.Documents.Module;
+import Model.user.User;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -13,73 +13,73 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class ActionManagerTest {
     Module m1;
     Module m2;
-    Teacher marcel;
-    Teacher jean;
+    User marcel;
+    User jean;
     Cours cours1;
     Cours cours2;
-    Student std1;
-    Student std2;
-    ActionsManager actionsManager = new ActionsManager();
+    User std1;
+    User std2;
+    //ActionsManager actionsManager = new ActionsManager();
 
     @Given("a module of name {string} with module id {string}")
     public void giveAModule(String moduleName, String moduleId){
-        m1 = new Module(moduleName, moduleId);
+        m1 = new Module(moduleName);
     }
 
     @Given("a teacher of name {string} and with teacher id {string}")
     public void giveATeacher(String name, String id){
-        marcel = new Teacher(name, id);
+        marcel = new User(name,name + ".fr","06");
     }
 
     @Given("a course of name {string} and with course id {string}")
     public void giveACourse(String name, String id){
-        cours1 = new Cours(name, id);
+        cours1 = new Cours(name);
     }
 
     @Given("a student of name {string} and with student id {string}")
     public void giveAStudent(String name, String id){
-        std1 = new Student(name, id);
+        std1 = new User(name,name + ".fr","06");
     }
 
     @And("a module of name {string} with module id {string}")
     public void andModule(String moduleName, String moduleId){
-        m2 = new Module(moduleName, moduleId);
+        m2 = new Module(moduleName);
     }
 
     @And("a teacher of name {string} and with teacher id {string}")
     public void andTeacher(String teacherName, String teacherId){
-        jean = new Teacher(teacherName, teacherId);
+        jean = new User(teacherName,teacherName + ".fr","06");
     }
 
     @And("a course of name {string} and with course id {string}")
     public void andCourse(String courseName, String courseId){
-        cours2 = new Cours(courseName, courseId);
+        cours2 = new Cours(courseName);
     }
 
     @And("a student of name {string} and with student id {string}")
     public void andStudent(String studentName, String studentId){
-        std2 = new Student(studentName, studentId);
+        std2 = new User(studentName, studentName + ".fr",studentId);
     }
 
     @When("{string} creates the course {string} in the module {string}")
     public void WhenCreateCourse(String teacherName, String courseName, String moduleId){
-        actionsManager.addCourse(moduleId, courseName);
+        //actionsManager.addCourse(moduleId, courseName);
     }
 
     @When("{string} creates the questionnaire {string} in the module {string}")
     public void WhenCreateQuestionnaire(String teacherName, String questionnaireName, String moduleId){
-        actionsManager.addQuestionnaireToModule(moduleId, questionnaireName);
+        //actionsManager.addQuestionnaireToModule(moduleId, questionnaireName);
     }
 
     @When("{string} add the question {string} : {string} to the questionnaire {string}, in the module {string}")
     public void WhenAddingAQuestionToAQuestionnaire(String teacherName, String question, String answer, String questionnaireName, String moduleName){
-        actionsManager.addQuestToQuestionaire(moduleName, questionnaireName, question, answer);
+        //actionsManager.addQuestToQuestionaire(moduleName, questionnaireName, question, answer);
     }
 
     @When("{string} register to the course {string}")
     public void WhenRegisterToCourse(String teacherName, String courseName){
         //actionsManager.assigneTeacher(courseName, teacherName);
-        actionsManager.setTeacher(courseName, teacherName);
+        //actionsManager.setTeacher(courseName, teacherName);
     }
 
     @Then("there is the course {string} in the right module")
