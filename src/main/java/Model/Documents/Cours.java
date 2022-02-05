3350098c5b6d236e0b5c235cf1df4d520b092049
KuhlMatthis <matthis.kuhl@etu.univ-cotedazur.fr>
2022-02-05 18:47:36 +0100

    commentaire @pour cours

--- src/main/java/Model/Documents/Cours.java
+++ src/main/java/Model/Documents/Cours.java
@@ -1,15 +1,27 @@
 package Model.Documents;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
-//@Entity
+@Entity
+@Table(	name = "Cours")
 public class Cours extends Ressource{
-    //@Id
-    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
 
-    public Set<String> text;
+    @NotBlank
+    public String name;
+
+
+    @ElementCollection
+    @CollectionTable(name="text", joinColumns=@JoinColumn(name="text_id"))
+    @Column(name="text_name")
+    private Set<String>  text = new HashSet<>();
 
 
     public Cours() { }
