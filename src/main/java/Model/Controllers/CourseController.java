d6b1839b8131306ac0570e201996e721573ffa45
louay belhassen <belhassenloulou@yahoo.fr>
2022-02-10 10:23:16 +0100

    update course Controller

--- src/main/java/Model/Controllers/CourseController.java
+++ src/main/java/Model/Controllers/CourseController.java
@@ -2,10 +2,9 @@ package Model.Controllers;
 
 
 import Model.Documents.Cours;
-import Model.Documents.Question;
-import Model.Documents.Questionnaire;
 import Model.Documents.Ressource;
 import Model.Payload.response.MessageResponse;
+import Model.Repositories.CoursesRepository;
 import Model.Repositories.RessourcesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +12,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
@@ -20,8 +20,21 @@ import java.util.Set;
 @RestController
 @RequestMapping("/api/module/course")
 public class CourseController {
+
+
+
     @Autowired
     RessourcesRepository ressourcesRepository;
+
+    @Autowired
+    CoursesRepository coursesRepository;
+
+
+
+
+
+
+
     @PutMapping("/{id}/question/{text}")
     @PreAuthorize("hasRole('TEACHER')")
     public ResponseEntity<?> addRessource(Principal principal, @PathVariable long id, @PathVariable String text){
@@ -82,6 +95,52 @@ public class CourseController {
         ressourcesRepository.save(ressource);
         return ResponseEntity.ok(new MessageResponse("User successfully added to cours!"));
     }
+
+
+
+
+
+    /**
+     * Read - Get all courses of  a student
+     * @return - An Iterable object of courses full filled
+     *
+     *
+     * Les utilisateurs peuvent connaitre la liste des cours sur lesquels ils sont inscrits
+     * à ajouter:
+     * verif that the student have acces to the courses in this module
+     *
+     */
+   @GetMapping("/api/{idStudent}/module/courses")
+    public List<Cours> getCourses(){
+
+       return coursesRepository.findAll();
+
+   }
+
+
+
+    /**
+     * Read - Get a course  of  a student
+     * @return - An course object
+     *
+     * Les utilisateurs peuvent connaitre la liste des cours sur lesquels ils sont inscrits
+     * à ajouter:
+     * verif that the student have acces to the courses in this module
+     *
+     *
+     *
+     */
+    @GetMapping("/api/{idStudent}/module/courses/{idCourse}")
+    public Optional<Cours> getCourse(final Long idCourse){
+
+        return coursesRepository.findById(idCourse);
+
+    }
+
+
+
+
+
 
 
 }
